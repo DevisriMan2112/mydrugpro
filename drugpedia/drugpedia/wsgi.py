8456a3fd18efcfa73abeccf425206b405f23a37c
devisrimani <subramaniandevisri7@gmail.com>
2024-08-10 12:15:20 +0530

    file upload

---
+++ drugpedia/drugpedia/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for drugpedia project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drugpedia.settings')
+
+application = get_wsgi_application()
